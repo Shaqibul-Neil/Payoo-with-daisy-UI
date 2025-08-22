a744068f2163917622173f19d057cc032bb35990
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-08-23 00:00:08 +0600

    add

---
+++ home.js
@@ -0,0 +1,12 @@
+//add money Section functionality
+//elements
+const currentBalance = document.querySelector(".current-balance");
+const selectBank = document.querySelector(".select");
+const inputAddAccount = document.querySelector(".input-add-account");
+const inputAddAmount = document.querySelector(".input-add-amount");
+const inputAddPin = document.querySelector(".input-add-pin");
+const btnAddMoney = document.querySelector("#btn-add-money");
+
+btnAddMoney.addEventListener("click", function (e) {
+  e.preventDefault();
+});
